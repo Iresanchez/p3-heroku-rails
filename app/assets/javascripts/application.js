0c555ff2f30c16877f3794eb830dbce5feb919a4
Irene <i.gonzalez102@usp.ceu.es>
2018-03-13 09:46:56 +0000

    foundation con jquery arreglado

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require foundation
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require foundation
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
